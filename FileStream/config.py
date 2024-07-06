ba3de8f367d6a2b96959d38b561b597bc3695bfe
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2024-07-06 12:01:34 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -27,7 +27,7 @@ class Telegram:
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
 
 class Server:
-    PORT = int(env.get("PORT", 3000))
+    PORT = int(env.get("PORT", 8080))
     BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
